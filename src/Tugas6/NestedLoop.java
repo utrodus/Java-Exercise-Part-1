87004d1da1b2ad3fdde4662a4c19101ed680064a
utrodus <come.programmer@gmail.com>
2019-04-23 20:52:07 +0700

    menambah segitiga sama kaki terbalik

--- src/Tugas6/NestedLoop.java
+++ src/Tugas6/NestedLoop.java
@@ -127,9 +127,39 @@ public class NestedLoop {
                         
             
             System.out.println(); // untuk membentuk spasi kebawah
-           
-
+                       
+        }
+        
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("Bentuk segitiga dengan bintang sama kaki terbalik");
+        System.out.println(" ");
+        
+        for(int x = 1; x<=6; x++){
+            //loop luar
             
+            for(int y = 1; y <=x; y++){
+                //loop dalam ke 1
+                System.out.print(" ");
+                // membentuk spasi kesamping
+            }
+            
+            for(int z = 6; z>=x; z--){
+                //loop dalam ke 2
+                System.out.print("*");
+                // untuk menampilkan barisan bintang terbalik dari kanan ke kiri
+            }
+            
+            
+            for(int a = 4; a>=x-1;a--){
+                //loop dalam ke 3
+                System.out.print("*");
+                // untuk menampilkan barisan bintang terbalik dari kiri ke kanan
+            }
+                        
+            
+            System.out.println(); // untuk membentuk spasi kebawah
+                       
         }
         
     }
